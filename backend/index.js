cf3ca70b416f3431197a4263f9fa450702057ad2
KennyVans <vansauskajtekaterina@gmail.com>
2025-06-07 08:49:06 +0300

    новые изменение , перенесена папка фронтенд

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,17 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-require('dotenv').config();
+const path = require('path'); 
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+// изменения именно тут 
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 const todoRoutes = require('./routes/todos');
 app.use('/api/todos', todoRoutes);
